4f3d142d13bd6fd2b46a470f5ae11202e25f6b4b
mby-wsl-kantor <raffie.arfa17@gmail.com>
2025-04-24 17:42:14 +0700

    doc(commit): Penjelasan Commit Fresh

--- commit-fresh.sh
+++ commit-fresh.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-git checkout --orphan temp_branch
-git add -A
-read -p "Enter commit message: " commit_message
-git commit -S -m "$commit_message"
-git branch -D master
-git branch -m master
-git push --force origin master
+git checkout --orphan temp_branch # temp branch
+git add -A # add all files
+read -p "Enter commit message: " commit_message # prompt commit message
+git commit -S -m "$commit_message" # set commit message
+git branch -D master # delete branch master
+git branch -m master # -- ntah --
+git push --force origin master # push (force) ke master
